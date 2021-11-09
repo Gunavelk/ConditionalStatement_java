dd37831a9bef7c174d43b78171404f212692f28e
Gunavelk <93586392+Gunavelk@users.noreply.github.com>
2021-11-09 14:35:06 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,11 @@
+
+public class Numbers {
+	public static void main(String args[]) {
+		int num = -7;
+		if (num >= 0) {
+			System.out.println("The Number is Positive");
+		} else {
+			System.out.println("The Number is Negative");
+		}
+	}
+}
